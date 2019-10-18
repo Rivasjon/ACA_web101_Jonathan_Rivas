f0e8e83f6defeaf60a3ef84b525c52241173d7d0
Rivasjon <Rivasjon000@Gmail.com>
2019-10-18 08:16:14 -0500

    clicker game

---
+++ src/checkpoint3/.js
@@ -0,0 +1,38 @@
+alert("Currently in beta,so under development^^");
+
+var x=0;
+var lv=1;
+function oncl(){
+   x+=lv;
+   textUpdate();
+    
+}
+function textUpdate(){
+    document.getElementById("p").innerHTML=x+" points";
+    
+};
+function upgrade(){
+   switch (lv) {
+      case 1:
+      if (x>=100){
+         lv++;
+         x-=100;   document.getElementById("u").innerHTML= "Upgrade for 200";
+      }
+      break;
+      case 2:
+      if (x>=200){
+         lv=4;
+         x-=200;   document.getElementById("u").innerHTML= "Upgrade for 500";
+       
+   }
+   break;
+   case 4:
+   
+if (x>=100){
+         lv=8;
+         x-=500;   document.getElementById("u").innerHTML= "Fully Upgraded";
+}
+break;
+}
+}
+
